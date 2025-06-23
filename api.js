5f5af5ca3f7f398d682f9b342595adce2f39e455
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-23 16:15:59 +0100

    Add api file to fetch all articles from the api

---
+++ api.js
@@ -0,0 +1,13 @@
+export function getAllArticles() {
+  return fetch(`https://nc-news-api-aoq3.onrender.com/api/articles`).then(
+    (res) => {
+      if (!res.ok) {
+        return Promise.reject({
+          status: res.status,
+          msg: "Failed to fetch articles",
+        });
+      }
+      return res.json().then((article) => article.articles);
+    }
+  );
+}
