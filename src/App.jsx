e1e701c11a1cf2074945868727e1967d12c14adc
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-24 15:52:54 +0100

    Import ArticleDetails component and Update to include the route for single article by id

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import ArticleList from "./ArticlesList";
+import ArticleDetails from "./ArticleDetails";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<ArticleList />} />
+        <Route path="/articles/:article_id" element={<ArticleDetails />} />
       </Routes>
     </>
   );
