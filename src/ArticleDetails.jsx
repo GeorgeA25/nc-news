f2101da3dd6f035af0ca06143e6d1e25fa5dd3fe
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-24 15:45:10 +0100

    Create ArticleDetails component to fetch and display a single article details using article_id from route params

---
+++ src/ArticleDetails.jsx
@@ -0,0 +1,68 @@
+import { useParams } from "react-router-dom";
+import { getArticleById } from "../api";
+import { useEffect, useState } from "react";
+import { formatDate } from "../utils/utils";
+
+function ArticleDetails() {
+    const [article, setArticle] = useState(null)
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const {article_id} = useParams()
+
+    useEffect(() => {
+        async function fetchArticleById() {
+            setLoading(true)
+            setError(null)
+            try{
+                const articleData = await getArticleById(id)
+                setArticle(articleData)
+            } catch (error) {
+        setError("Failed to load article");
+      } finally {
+        setLoading(false);
+      }
+    }
+        fetchArticleById()
+}, [article_id])
+
+
+
+if (loading) {
+    return <p>Loading ...</p>
+}
+
+if(error) {
+    return <p>{error}</p>
+}
+
+if(!article) {
+    return <p>No article found on {id}</p>
+}
+
+const {
+    title,
+    author,
+    topic,
+    body,
+    comment_count,
+    votes,
+    created_at,
+    article_img_url,
+  } = article;
+
+return (
+     <article className="article-card">
+      <img src={article_img_url} alt={`Image for article titled "${title}"`} />
+      <h3>{title}</h3>
+      <p>{body}</p>
+      <p>Author: {author}</p>
+      <p>Topic: {topic}</p>
+      <p>Comments: {comment_count}</p>
+      <p>Votes: {votes}</p>
+      <p>Published: {formatDate(created_at)}</p>
+      </article>
+)
+
+}
+
+export default ArticleDetails
