9ec904be35553d717fa5f4904e3b7d9fca6acd2c
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-24 11:42:44 +0100

    Create Header component with Link to the home route by importing Link from react-router-dom

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="site-header">
+      <h1>NC News</h1>
+      <nav className="nav">
+        <ul className="nav-list">
+          <Link to="/"> Home</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
