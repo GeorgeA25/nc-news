844e74bfdb18dec2c6238bec4bb77a02a62e6acc
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-24 12:17:18 +0100

    Add getArticlesById function to fetch an article by it's id

--- api.js
+++ api.js
@@ -13,3 +13,16 @@ export function getAllArticles() {
     }
   );
 }
+
+export function getArticleById(id) {
+  return fetch(`https://nc-news-api-aoq3.onrender.com/api/articles/${id}`).then(
+    (res) => {
+      if (!res.ok) {
+        return Promise.reject({
+          status: res.status,
+          msg: "Failed to fetch article by id",
+        });
+      }
+      return res.json()
+      });
+    }
