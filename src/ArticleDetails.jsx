1769609db5259a0b1e6b691d16226939387e23c8
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-25 14:07:17 +0100

    Import CommentCard child component into ArticleDetails, add states for comments , commentsError and commentsLoading, the create new use affect for fetching comments by id and handle loading and erros. Update return statement to include loading and erros and then map over comments to display all comments for an article based off it's id. Import util function to format the date of the comments posted for readability  and getCommentsbyId function

--- src/ArticleDetails.jsx
+++ src/ArticleDetails.jsx
@@ -1,43 +1,65 @@
 import { useParams } from "react-router-dom";
-import { getArticleById } from "../api";
+import { getArticleById, getCommentsById } from "../api";
 import { useEffect, useState } from "react";
 import { formatDate } from "../utils/utils";
+import CommentCard from "./CommentCard";
 
 function ArticleDetails() {
     const [article, setArticle] = useState(null)
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [articleLoading, setArticleLoading] = useState(true);
+    const [articleError, setArticleError] = useState(null);
+    const [comments, setComments] = useState([])
+    const [commentsLoading, setCommentsLoading] = useState(true);
+    const [commentsError, setCommentsError] = useState(null)
     const {article_id} = useParams()
 
     useEffect(() => {
         async function fetchArticleById() {
-            setLoading(true)
-            setError(null)
+            setArticleLoading(true)
+            setArticleError(null)
             try{
                 const articleData = await getArticleById(article_id)
                 setArticle(articleData)
             } catch (error) {
-        setError("Failed to load article");
+        setArticleError("Failed to load article or comments");
       } finally {
-        setLoading(false);
+        setArticleLoading(false);
       }
     }
-        fetchArticleById()
+    fetchArticleById()
+}, [article_id])   ; 
+    
+    useEffect(() => {
+         async function fetchCommentsById() {
+            setCommentsLoading(true)
+            setCommentsError(null)
+            try{
+                const commentData = await getCommentsById(article_id)
+                setComments(commentData)
+            } catch (error) {
+        setCommentsError("Failed to load comments by id");
+      } finally {
+        setCommentsLoading(false);
+      }
+    }
+        fetchCommentsById()
+
 }, [article_id])
 
 
-
-if (loading) {
+if (articleLoading) {
     return <p>Loading ...</p>
 }
 
-if(error) {
-    return <p>{error}</p>
+if(articleError) {
+    return <p>{articleError}</p>
 }
 
 if(!article) {
-    return <p>No article found on {id}</p>
+    return <p>No article found on {article_id}</p>
 }
+
+
 
 const {
     title,
@@ -51,7 +73,8 @@ const {
   } = article;
 
 return (
-    <section>
+    <>
+    <section className="article-details">
     <h2>{title}</h2>
       <img src={article_img_url} alt={`Image for article titled "${title}"`} />
       <p>Body: {body}</p>
@@ -61,6 +84,28 @@ return (
       <p>Votes: {votes}</p>
       <p>Published: {formatDate(created_at)}</p>
      </section> 
+    <section>
+
+    <h3>Comments</h3>
+    {commentsLoading ? (
+        <p> Loading comments ...</p>
+    ) : commentsError ? 
+    (    <p>{commentsError}</p>
+    ) : comments.length === 0 ? (
+        <p>No comments yet </p>
+    ) : (
+       <section className="comments-list">
+        <ul>
+        {comments.map((comment) => (
+         <li key={comment.comment_id}>
+          <CommentCard  comment={comment} />
+        </li> 
+        ))}
+        </ul>
+     </section>
+)}
+     </section>
+    </> 
 )
 
 }
