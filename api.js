ebaa9fdb1f414b5e59469561aa88a25fd37b4186
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-25 09:52:44 +0100

    Add getCommentsById function to fetch comments by article_id

--- api.js
+++ api.js
@@ -28,3 +28,18 @@ export function getArticleById(article_id) {
       })
       });
     }
+
+ export function getCommentsById(article_id) {
+  return fetch(`https://nc-news-api-aoq3.onrender.com/api/articles/${article_id}/comments`).then(
+    (res) => {
+      if (!res.ok) {
+        return Promise.reject({
+          status: res.status,
+          msg: "Failed to fetch comments by id",
+        });
+      }
+      return res.json().then(({comments}) => {
+        return comments
+      })
+      });
+    }   
