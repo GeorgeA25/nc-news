da605e801174f9fb26d1c692c2fc767efc2cb321
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-26 16:43:36 +0100

    Refactor to only handle post comments onto an article's page

--- src/CommentForm.jsx
+++ src/CommentForm.jsx
@@ -1,44 +1,39 @@
 import { useState } from "react";
 import { postCommentToArticle } from "../api";
 
-function CommentForm({article_id, users, addComment, onPostError}) {
-    const [selectedUser, setSelectedUser] = useState("")
-    const [commentInput, setCommentInput] = useState("")
+function CommentForm({ article_id, addComment, onPostError, selectedUser }) {
+  const [commentInput, setCommentInput] = useState("");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-try {
-        const newComment = await postCommentToArticle(article_id, {username: selectedUser, body: commentInput})
-        addComment(newComment)
-        setCommentInput("")
-        setSelectedUser("")
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const newComment = await postCommentToArticle(article_id, {
+        username: selectedUser,
+        body: commentInput,
+      });
+      addComment(newComment);
+      setCommentInput("");
     } catch (error) {
-        onPostError("Failed to post comment")
-     }}
-    const handleChange = (event) => {
-        const {id, value} = event.target
-        if(id === "user-select") {
-            setSelectedUser(value)
-        } else if (id === "comment-input") {
-            setCommentInput(value)
-        }
+      onPostError("Failed to post comment");
     }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="user-select">Choose User</label>
-            <select id="user-select" value={selectedUser} onChange={handleChange} required>
-                <option value="" disabled> Select a user</option>
-                {users.map(({username}) => (
-                    <option key={username} value={username}>{username}</option>
-                ))}
-            </select>
-            <label htmlFor="comment-input">Your Comment:</label>
-            <textarea id="comment-input" value={commentInput} onChange={handleChange} />
-            {!commentInput.trim() && (<p>Please write a comment</p>)}
-            <button type="submit" disabled={!selectedUser || !commentInput.trim()}>Add Comment</button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="comment-input">Your Comment:</label>
+      <textarea
+        id="comment-input"
+        placeholder="Please write comment"
+        value={commentInput}
+        onChange={(event) => setCommentInput(event.target.value)}
+        maxLength={100}
+      />
+      {!commentInput.trim() && <p>Please write a comment</p>}
+      <button type="submit" disabled={!selectedUser || !commentInput.trim()}>
+        Add Comment
+      </button>
+    </form>
+  );
 }
 
-export default CommentForm
+export default CommentForm;
