fb5ff79f9bdeae5791c4bfdfa012a49d8dcc8a7a
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-26 16:51:16 +0100

    Refactor to handle user selection in commentManager component so that a comment could be posted or deleted based off the selected user from the drop down box and display loading and error messages based on user input

--- src/CommentsManager.jsx
+++ src/CommentsManager.jsx
@@ -1,107 +1,167 @@
 import { useEffect, useState } from "react";
 import CommentCard from "./CommentCard";
-import { getCommentsById, getUsers} from "../api";
+import { deleteCommentById, getCommentsById, getUsers } from "../api";
 import CommentForm from "./CommentForm";
 
+function CommentManager({ article_id }) {
+  const [comments, setComments] = useState([]);
+  const [commentsLoading, setCommentsLoading] = useState(true);
+  const [commentsError, setCommentsError] = useState(null);
+  const [userLoading, setUserLoading] = useState(true);
+  const [userLoadError, setUserLoadError] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
+  const [commentPostSuccessMessage, setCommentPostSuccessMessage] =
+    useState(null);
+  const [commentPostErrorMessage, setCommentPostErrorMessage] = useState(null);
+  const [deleteLoading, setDeletingLoading] = useState(null);
+  const [deleteSuccessMessage, setDeleteSuccessMessage] = useState(null);
+  const [deleteErrorMessage, setDeleteErrorMessage] = useState(null);
 
-function CommentManager({article_id}) {
-const [comments, setComments] = useState([])
-const [commentsLoading, setCommentsLoading] = useState(true);
-const [commentsError, setCommentsError] = useState(null)
-const [userLoading, setUserLoading] = useState(true)
-const [userLoadError, setUserLoadError] = useState(null)
-const [users, setUsers] = useState([])
-const [commentPostSuccessMessage, setCommentPostSuccessMessage] = useState(null)
-const [commentPostErrorMessage, setCommentPostErrorMessage] = useState(null)
-
- useEffect(() => {
-         async function fetchCommentsById() {
-            setCommentsLoading(true)
-            setCommentsError(null)
-            try{
-                const commentData = await getCommentsById(article_id)
-                setComments(commentData)
-            } catch (error) {
+  useEffect(() => {
+    async function fetchCommentsById() {
+      setCommentsLoading(true);
+      setCommentsError(null);
+      try {
+        const commentData = await getCommentsById(article_id);
+        setComments(commentData);
+      } catch (error) {
         setCommentsError("Failed to load comments by id");
       } finally {
         setCommentsLoading(false);
       }
     }
-        fetchCommentsById()
+    fetchCommentsById();
+  }, [article_id]);
 
-}, [article_id])
-
- useEffect(() => {
+  useEffect(() => {
     async function fetchUsers() {
-        setUserLoading(true)
-        setUserLoadError(null)
-        try {
-            const userData = await getUsers()
-            setUsers(userData)
-        } catch (error) {
-            setUserLoadError("Failed to load users")
-        } finally {
-            setUserLoading(false)
-        }
+      setUserLoading(true);
+      setUserLoadError(null);
+      try {
+        const userData = await getUsers();
+        setUsers(userData);
+      } catch (error) {
+        setUserLoadError("Failed to load users");
+      } finally {
+        setUserLoading(false);
+      }
     }
-    fetchUsers()
- }, [])
+    fetchUsers();
+  }, []);
 
-const addComment = (newComment) => {
-    setComments((currentComments) => [newComment, ...currentComments])
-    setCommentPostSuccessMessage("Comment posted successfully")
-    setCommentPostErrorMessage(null)
-    setTimeout(() => setCommentPostSuccessMessage(null), 5000)
-}
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    if (id === "user-select") {
+      setSelectedUser(value);
+    } else if (id === "comment-input") {
+      setCommentInput(value);
+    }
+  };
 
-const handleCommentPostError = (error) => {
-    setCommentPostErrorMessage(error)
-    setCommentPostSuccessMessage(null)
-    setTimeout(() => setCommentPostErrorMessage(null), 5000)
-}
+  const addComment = (newComment) => {
+    setComments((currentComments) => [newComment, ...currentComments]);
+    setCommentPostSuccessMessage("Comment posted successfully");
+    setCommentPostErrorMessage(null);
+    setTimeout(() => setCommentPostSuccessMessage(null), 5000);
+  };
 
-return (
-<>
-     <section>
-    <h3>Comments</h3>
-<section className="post-and-error-messages">
-    {commentPostSuccessMessage && <p>{commentPostSuccessMessage}</p>}
-    {commentPostErrorMessage && <p>{commentPostErrorMessage}</p>}
-</section>
-    {commentsLoading ? (
-        <p> Loading comments ...</p>
-    ) : commentsError ? 
-    (    <p>{commentsError}</p>
-    ) : comments.length === 0 ? (
-        <p>No comments yet </p>
-    ) : (
-       <section className="comments-list">
-        <ul>
-        {comments.map((comment) => (
-         <li key={comment.comment_id}>
-          <CommentCard  comment={comment} />
-        </li> 
-        ))}
-        </ul>
-   </section>
-    )}
-    <section className="user-loading-error">
-    {userLoading && <p>Loading users ...</p>}
-    {userLoadError && <p>{userLoadError}</p>}
-</section>
-<section className="comment-form-section">
-    {!userLoading && !userLoadError && (
-        <CommentForm
-        article_id={article_id}
-        users={users}
-        addComment={addComment}
-        onPostError={handleCommentPostError}
-        />
-    )}
-     </section>
-    </section>
+  const handleCommentPostError = (error) => {
+    setCommentPostErrorMessage(error);
+    setCommentPostSuccessMessage(null);
+    setTimeout(() => setCommentPostErrorMessage(null), 5000);
+  };
+
+  const handleDeleteComment = async (commentId) => {
+    try {
+      setDeletingLoading(commentId);
+      await deleteCommentById(commentId);
+      setComments((currentComments) => {
+        return currentComments.filter(
+          (comment) => comment.comment_id !== commentId
+        );
+      });
+      setDeleteSuccessMessage(" Your comment has been successfully deleted");
+      setDeleteErrorMessage(null);
+      setTimeout(() => setDeleteSuccessMessage(null), 5000);
+    } catch (errro) {
+      setDeleteErrorMessage("Failed to delete your comment");
+      setDeleteSuccessMessage(null);
+    } finally {
+      setDeletingLoading(null);
+    }
+  };
+
+  return (
+    <>
+      <section>
+        <h3>Comments</h3>
+        <section className="post-and-error-messages">
+          {commentPostSuccessMessage && <p>{commentPostSuccessMessage}</p>}
+          {commentPostErrorMessage && <p>{commentPostErrorMessage}</p>}
+          {deleteSuccessMessage && <p>{deleteSuccessMessage}</p>}
+          {deleteErrorMessage && <p>{deleteErrorMessage}</p>}
+        </section>
+        {commentsLoading ? (
+          <p> Loading comments ...</p>
+        ) : commentsError ? (
+          <p>{commentsError}</p>
+        ) : comments.length === 0 ? (
+          <p>No comments yet </p>
+        ) : (
+          <section className="comments-list">
+            <ul>
+              {comments.map((comment) => (
+                <li key={comment.comment_id}>
+                  <CommentCard
+                    comment={comment}
+                    onDelete={handleDeleteComment}
+                    deleting={deleteLoading === comment.comment_id}
+                    selectedUser={selectedUser}
+                  />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+        <section className="user-loading-error">
+          {userLoading && <p>Loading users ...</p>}
+          {userLoadError && <p>{userLoadError}</p>}
+        </section>
+        <section className="user-selector">
+          <label htmlFor="user-select">Choose User</label>
+          <select
+            id="user-select"
+            value={selectedUser}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>
+              {" "}
+              Select a user
+            </option>
+            {users.map(({ username }) => (
+              <option key={username} value={username}>
+                {username}
+              </option>
+            ))}
+          </select>
+        </section>
+        <section className="comment-form-section">
+          {!userLoading && !userLoadError && (
+            <CommentForm
+              article_id={article_id}
+              users={users}
+              addComment={addComment}
+              onPostError={handleCommentPostError}
+              selectedUser={selectedUser}
+              setSelectedUser={setSelectedUser}
+            />
+          )}
+        </section>
+      </section>
     </>
-)}
+  );
+}
 
-
-export default CommentManager
+export default CommentManager;
