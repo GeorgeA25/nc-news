0bf3cc5cd4e953039a2167a5b20b2c6fd8823fa4
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-26 18:43:01 +0100

    Create TopicDetails to fetch and display a list of articles by topic

---
+++ src/TopicDetails.jsx
@@ -0,0 +1,54 @@
+import { useParams } from "react-router-dom";
+import { getArticleByTopic } from "../api";
+import { useEffect, useState } from "react";
+import ArticleCard from "./ArticleCard";
+
+function TopicDetails() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    async function fetchArticleByTopic() {
+      setLoading(true);
+      setError(null);
+      try {
+        const articleData = await getArticleByTopic(slug);
+        setArticles(articleData);
+      } catch (error) {
+        setError("Failed to load articles for this topic");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchArticleByTopic();
+  }, [slug]);
+
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!articles.length) {
+    return <p>No article found on topic {slug}</p>;
+  }
+
+  return (
+    <>
+      <main>
+        <h2>Articles on {slug}</h2>
+        <section className="article-list">
+          {articles.map((article) => (
+            <ArticleCard key={article.article_id} article={article} />
+          ))}
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default TopicDetails;
