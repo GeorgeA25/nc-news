b41af55acc80b33f8d411b17e77c52f4edbda485
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-26 18:08:33 +0100

    Update to import TopicsList component,  add a route for topics page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import ArticleList from "./ArticlesList";
 import ArticleDetails from "./ArticleDetails";
+import TopicsList from "./TopicsPage";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ArticleList />} />
         <Route path="/articles/:article_id" element={<ArticleDetails />} />
+        <Route path="/topics" element={<TopicsList />} />
       </Routes>
     </>
   );
