9a4d1f464710604ef23e0585b8213066d49c474c
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-27 15:32:25 +0100

    Update import the the patch function and commentCard component to display the comments assiociated with the selected article, then apply more states for votes, loading and error messages on votes. Then add a handle vote function to handle vote count to upvote or downvote by optimistic rendering so user sees vote change immediatly and then create two buttons to handle the on click of either upvote or downvote

--- src/ArticleDetails.jsx
+++ src/ArticleDetails.jsx
@@ -1,17 +1,18 @@
 import { useParams } from "react-router-dom";
-import { getArticleById, getCommentsById } from "../api";
+import { getArticleById, patchArticleVotes } from "../api";
 import { useEffect, useState } from "react";
 import { formatDate } from "../utils/utils";
-import CommentCard from "./CommentCard";
+import CommentManager from "./CommentsManager";
 
 function ArticleDetails() {
     const [article, setArticle] = useState(null)
     const [articleLoading, setArticleLoading] = useState(true);
     const [articleError, setArticleError] = useState(null);
-    const [comments, setComments] = useState([])
-    const [commentsLoading, setCommentsLoading] = useState(true);
-    const [commentsError, setCommentsError] = useState(null)
     const {article_id} = useParams()
+    const [votesCount, setVotesCount] = useState(0)
+    const [votesCountError, setVotesCountError] = useState(null)
+    const [votesSuccessMessage, setVotesSuccessMessage] = useState(null)
+    const [isVoteLoading, setIsVoteLoading] = useState(false)
 
     useEffect(() => {
         async function fetchArticleById() {
@@ -20,6 +21,7 @@ function ArticleDetails() {
             try{
                 const articleData = await getArticleById(article_id)
                 setArticle(articleData)
+                setVotesCount(articleData.votes)
             } catch (error) {
         setArticleError("Failed to load article or comments");
       } finally {
@@ -27,24 +29,8 @@ function ArticleDetails() {
       }
     }
     fetchArticleById()
-}, [article_id])   ; 
-    
-    useEffect(() => {
-         async function fetchCommentsById() {
-            setCommentsLoading(true)
-            setCommentsError(null)
-            try{
-                const commentData = await getCommentsById(article_id)
-                setComments(commentData)
-            } catch (error) {
-        setCommentsError("Failed to load comments by id");
-      } finally {
-        setCommentsLoading(false);
-      }
-    }
-        fetchCommentsById()
+}, [article_id]); 
 
-}, [article_id])
 
 
 if (articleLoading) {
@@ -72,6 +58,27 @@ const {
     article_img_url,
   } = article;
 
+
+const handleVote = async (inc_votes) => {
+    
+if(isVoteLoading) return
+setIsVoteLoading(true)
+
+    setVotesCount((currentCount) => currentCount + inc_votes)
+    setVotesCountError(null)
+    setVotesSuccessMessage(null)
+    try {
+    await patchArticleVotes(article_id, inc_votes)
+    setVotesSuccessMessage("Your vote was successfully submitted!")
+    setTimeout(() => setVotesSuccessMessage(null), 5000)
+}   catch (error) {
+    setVotesCount((currentCount) => currentCount - inc_votes)
+    setVotesCountError("Failed to submit your vote, Please try again")
+}   finally{
+    setIsVoteLoading(false)
+}
+}
+
 return (
     <>
     <section className="article-details">
@@ -81,33 +88,18 @@ return (
       <p>Author: {author}</p>
       <p>Topic: {topic}</p>
       <p>Comments: {comment_count}</p>
-      <p>Votes: {votes}</p>
+      <p>Votes: {votesCount}</p>
+      <button disabled={isVoteLoading} onClick={() => handleVote(1)}>Upvote</button>
+       <button disabled={isVoteLoading} onClick={() => handleVote(-1)}>Downvote</button>
+       {votesCountError && <p className="error">{votesCountError}</p>}
+       {votesSuccessMessage && <p className="success">{votesSuccessMessage}</p>}
       <p>Published: {formatDate(created_at)}</p>
      </section> 
     <section>
-
-    <h3>Comments</h3>
-    {commentsLoading ? (
-        <p> Loading comments ...</p>
-    ) : commentsError ? 
-    (    <p>{commentsError}</p>
-    ) : comments.length === 0 ? (
-        <p>No comments yet </p>
-    ) : (
-       <section className="comments-list">
-        <ul>
-        {comments.map((comment) => (
-         <li key={comment.comment_id}>
-          <CommentCard  comment={comment} />
-        </li> 
-        ))}
-        </ul>
-     </section>
-)}
+        <CommentManager article_id={article_id} />
      </section>
     </> 
 )
-
 }
 
 export default ArticleDetails
